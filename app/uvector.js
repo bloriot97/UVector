7b722c293cfbe1b7baf515114ed9df9b1c5e2c08
Guillaume Jounel <gjounel@gmail.com>
2018-03-27 16:04:04 +0200

    Added index template

--- app/uvector.js
+++ app/uvector.js
@@ -6,14 +6,25 @@ var app = express();
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.use('/', [
+app.use('/static/', [
     express.static(__dirname + '/node_modules/materialize-css/dist/'),
+    express.static(__dirname + '/static/'),
 ]);
 
 
 app.get('/', (req, res) => {
-    res.render('index', {test: 'Hello world!'})
+    res.render('index', {test: req.protocol + '://' + req.get('host') +req.originalUrl})
 })
+
+// app.get('/login/', (req, res) => {
+//     res.redirect('https://cas.utc.fr/cas/login?service=' + req.protocol + '://' + req.get('host') + '/test')
+// })
+//
+// app.get('/test/', (req, res) => {
+//     var CAS = require('cas');
+//     var cas = new CAS({base_url: 'https://cas.utc.fr/cas/serviceValidate?service=', service: req.protocol + '://' + req.get('host') +req.originalUrl});
+//
+// })
 //
 // var neo4j = require('node-neo4j');
 // var db = new neo4j('http://neo4j:test@neo4j:7474');
